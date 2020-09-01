a1a56c54b8a83d0e639e8ff49dd721a0cbe96b30
FoxAli <peng1000@live.cn>
2020-09-01 17:15:07 +0800

    (@@)

--- DNSCenter.cpp
+++ DNSCenter.cpp
@@ -20,6 +20,8 @@ DNSCenter* CreateDNSCenter()
 {
 	DNSCenter* center = (DNSCenter*)malloc(sizeof(DNSCenter));
 	memset(center , 0 , sizeof(DNSCenter));
+
+	return center;
 }
 
 void ReleaseDNSCenter(DNSCenter* center)
