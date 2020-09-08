43f8770271a13a1091b267169d17787ed3333eb5
FoxAli <peng1000@live.cn>
2020-09-08 15:42:38 +0800

    凸(^_^)凸

--- Downloader.h
+++ Downloader.h
@@ -1,8 +1,19 @@
 #ifndef DOWNLOADER_H
 #define DOWNLOADER_H
 
+//#include "stdHeader.h"
 
-
+//http(s)://username:password@api.somesite.com/test/blah?something=123
+struct Url
+{
+	char* Protocol; //"http" or "https"
+	char* User;
+	char* Password;
+	char* Domain;
+	char* IP;
+	int Port;
+	char* Directory;
+};
 
 
 
--- husky.cpp
+++ husky.cpp
@@ -7,18 +7,7 @@
 #include "stdHeader.h"
 #include "DNSCenter.h"
 #include "ctype.h"
-
-//http(s)://username:password@api.somesite.com/test/blah?something=123
-struct Url
-{
-	char* Protocol; //"http" or "https"
-	char* User;
-	char* Password;
-	char* Domain;
-	char* IP;
-	int Port;
-	char* Directory;
-};
+#include "Downloader.h"
 
 #define SAFE_FREE(p) \
 	if(p)free(p); \
