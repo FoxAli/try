5a4287d7acf59782e238a1a6012723249439e507
FoxAli <peng1000@live.cn>
2020-09-15 11:21:21 +0800

    fix bug

--- Downloader.cpp
+++ Downloader.cpp
@@ -4,6 +4,7 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 #include "stdHeader.h" 
 
@@ -40,20 +41,57 @@ int Connect(int* sock , char* ip , int port)
 	return 0;
 }
 
-int SendRequest(Url* pUrl)
+int SendRequest(int* sock, Url* pUrl)
 {
+	char request[1024] = {0};
+	
+	sprintf(request, "GET /%s HTTP/1.0\r\n"
+		"Host: %s\r\n"
+		"Accept: */*\r\n"
+		"Connection: Keep-Alive\r\n"
+		"User-Agent: Mozilla/5.0 (compatible; Qteqpidspider/1.0;)\r\n"
+		"Referer: %s\r\n\r\n", pUrl->Directory, pUrl->Domain, pUrl->Domain);
+
+	int need, begin, n;
+	need = strlen(request);
+	begin = 0;
+	while (need) 
+	{
+		n = write(*sock, request + begin, need);
+		if (n <= 0) 
+		{
+			if (errno == EAGAIN) 
+			{ 
+				//write buffer full, delay retry
+				usleep(1000);
+				continue;
+			}
+			LOG(LOG_DEBUG, "Fail to send request.");
+			close(*sock);
+			return -1;
+		}
+		begin += n;
+		need -= n;
+	}
+
+	LOG(LOG_DEBUG, "Success to send request.");
+
 	return 0;
 }
 
 
 void DownLoad(Url* pUrl)
 {
-	int respSocket = 0;
-	if(0 != Connect(&respSocket , pUrl->IP , pUrl->Port))
+	int sock = 0;
+	if(0 != Connect(&sock, pUrl->IP , pUrl->Port))
 	{
 		return;
 	}
 
+	if (0 != SendRequest(&sock, pUrl))
+	{
+		return;
+	}
 	
 	
 }
